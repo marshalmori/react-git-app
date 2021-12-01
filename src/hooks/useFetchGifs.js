fc4f897d9afd2cd4743831df9a527bbe0ee91231
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2021-11-30 23:14:06 -0300

    Fix useFetchGifs.js

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useEffect } from "react/cjs/react.development";
+import { useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const useFetchGifs = (category) => {
