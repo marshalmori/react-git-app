9814b693d291244c09a86ea2938044032e7ec5fd
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2021-11-30 22:31:31 -0300

    Initial commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
+
+export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["Iron man"]);
+
+  return (
+    <>
+      <h1>Gif App</h1>
+      <AddCategory setCategories={setCategories} />
+      <hr />
+
+      <ol>
+        {categories.map((category) => (
+          <GifGrid key={category} category={category} />
+        ))}
+      </ol>
+    </>
+  );
+};
---
+++ src/hooks/useFetchGifs.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { useEffect } from "react/cjs/react.development";
+import { getGifs } from "../helpers/getGifs";
+
+export const useFetchGifs = (category) => {
+  const [state, setState] = useState({
+    data: [],
+    loading: true,
+  });
+
+  useEffect(() => {
+    getGifs(category).then((imgs) => {
+      setState({
+        data: imgs,
+        loading: false,
+      });
+    });
+  }, [category]);
+
+  return state; // O retorno state são os dados que estão no useState ==> {data: [], loading: true,}
+};
